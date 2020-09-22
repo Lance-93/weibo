f3bff99a73e914043e495089cdea2c865ec67fe1
sally <glen92093@gmail.com>
2020-09-22 08:22:40 +0000

    用户激活

---
+++ resources/views/emails/confirm.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>确认注册链接</title>
+</head>
+<body>
+<h1>think you in wei bo apps register for email account!</h1>
+<p>
+  点击下方链接完成注册
+  <a href="{{ route('confirm_email',$user->activation_token) }}">{{ route('confirm_email',$user->activation_token) }}
+  </a>
+</p>
+<p>
+  如果这不是您本人的操作，请忽略此邮件。
+</p>
+</body>
+</html>
